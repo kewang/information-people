464b3013e2536d919ce9e3b96e44733d85622ca0
kewang <cpckewang@gmail.com>
2016-02-21 22:52:42 +0800

    Add issues endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/issues', function(req, res, next) {
+  return res.end(req);
 });
 
 module.exports = router;
