a75a1e4ec9d4e9a02aec37bbdef4a4b06986bc2a
kewang <cpckewang@gmail.com>
2016-02-23 00:13:01 +0800

    Get gh-pages index.htm

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,16 @@ var Git = require("nodegit");
 var router = express.Router();
 
 router.post('/issues', function(req, res, next) {
-  Git.Clone("https://github.com/kewang/information-people", "./tmp").then(function(repo){
+  Git.Clone("https://github.com/kewang/information-people", "./tmp", {
+    checkoutBranch: "gh-pages"
+  }).then(function(repo){
     console.log("getHeadCommit");
 
     return repo.getHeadCommit();
   }).then(function(commit){
     console.log("getEntry");
 
-    return commit.getEntry("README.md");
+    return commit.getEntry("index.htm");
   }).then(function(entry){
     console.log("getBlob");
 
