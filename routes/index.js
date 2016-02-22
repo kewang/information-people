dc6ed5848d8bc6bcd7a3ae1b4d12eeec967b6e96
kewang <cpckewang@gmail.com>
2016-02-22 23:49:04 +0800

    Test nodegit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,27 @@
 var express = require('express');
+var Git = require("nodegit");
 var router = express.Router();
 
 router.post('/issues', function(req, res, next) {
-  return res.json(req.body);
+  Git.Clone("https://github.com/kewang/information-people", "./tmp").then(function(repo){
+    console.log("getHeadCommit");
+
+    return repo.getHeadCommit();
+  }).then(function(commit){
+    console.log("getEntry");
+
+    return commit.getEntry("README.md");
+  }).then(function(entry){
+    console.log("getBlob");
+
+    return entry.getBlob();
+  }).then(function(blob){
+    var str = String(blob);
+
+    console.log(str);
+
+    return res.json(str);
+  });
 });
 
 module.exports = router;
