49b3fb5255e9234d7817ce73a7b448372090bd17
kewang <cpckewang@gmail.com>
2016-02-24 02:39:37 +0800

    Add push flow, but test fail

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,63 @@
 var express = require('express');
 var Git = require("nodegit");
+var fs = require('fs');
 var router = express.Router();
-var token = process.env.GITHUB_TOKEN;
+var GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+var FILE_NAME = "index.htm";
+var content;
+var repo;
+var head;
+var oid;
 
 router.post('/issues', function(req, res, next) {
-  Git.Clone("https://" + token + ":x-oauth-basic@github.com/kewang/information-people", "./tmp", {
+  Git.Clone("https://" + GITHUB_TOKEN + ":x-oauth-basic@github.com/kewang/information-people", "./tmp", {
     checkoutBranch: "gh-pages"
-  }).then(function(repo){
+  }).then(function(repoResult){
+    repo = repoResult;
+
     console.log("getHeadCommit");
 
     return repo.getHeadCommit();
-  }).then(function(commit){
+  }).then(function(headResult){
+    head = headResult;
     console.log("getEntry");
 
-    return commit.getEntry("index.htm");
+    return head.getEntry(FILE_NAME);
   }).then(function(entry){
     console.log("getBlob");
 
     return entry.getBlob();
   }).then(function(blob){
-    var str = String(blob);
+    content = "DEF" + String(blob) + "ABC";
 
-    console.log(str);
+    fs.writeFileSync("./tmp/" + FILE_NAME, content);
 
-    return res.json(str);
+    return repo.index();
+  }).then(function(index){
+    index.addByPath(FILE_NAME);
+    index.write();
+
+    return index.writeTree();
+  }).then(function(oidResult){
+    oid = oidResult;
+
+    var author = Git.Signature.now("kewang", "cpckewang@gmail.com");
+
+    return repo.createCommit("HEAD", author, author, "Add test", oid, [head]);
+  }).then(function(commitId){
+    console.log("New Commit: " + commitId);
+
+    var remote = repo.getRemote("origin");
+
+    // var remote = Git.Remote.create(repo, "origin", "https://" + GITHUB_TOKEN + ":x-oauth-basic@github.com/kewang/information-people");
+
+    console.log("remote: " + remote);
+
+    return remote.push(["refs/heads/master:refs/heads/master"]);
+  }).done(function(){
+    console.log("Push OK");
+
+    return res.json(content);
   });
 });
 
