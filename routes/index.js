7bbd9ec608758b3a56d77e74f71c662b3a8ffa3c
kewang <cpckewang@gmail.com>
2016-02-27 20:12:22 +0800

    Add log

--- routes/index.js
+++ routes/index.js
@@ -38,6 +38,8 @@ router.post('/issues', function(req, res, next) {
 
     result = processing(req.body);
 
+    console.log("test: " + JSON.stringify(result));
+
     fs.writeFileSync(FOLDER_NAME + "/" + FILE_NAME, $.html());
 
     return repo.index();
